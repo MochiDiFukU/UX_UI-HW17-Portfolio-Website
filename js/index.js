eca51f77ffdcebe93a9b1d237f80efafa7189942
Pauline Martinez <89546541+MochiDiFunkU@users.noreply.github.com>
2022-03-01 16:11:02 -0700

    Updated Site Code For HW 19
    added butons

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-window.onscroll = function() {myFunction()};
-
-var nav = document.getElementById("nav");
-var sticky = navlist.offsetTop;
-
-/* Function to stick the nav bar */
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        nav.classList.add("sticky")
-    }
-    else {
-        nav.classList.remove("sticky");
-    }
-}
 
